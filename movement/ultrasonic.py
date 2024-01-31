6e159c1c9c091752fb3791288e7582b42622e520
TehDucctator <ducky543210@gmail.com>
2024-01-30 23:04:59 -0500

    Create ultrasonic.py to read manually given pins

---
+++ movement/ultrasonic.py
@@ -0,0 +1,51 @@
+import RPi.GPIO as GPIO
+import time
+ 
+#GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+ 
+#set GPIO Pins
+GPIO_TRIGGER = 18
+GPIO_ECHO = 24
+ 
+def setup_ultrasonic(TRIGGER, ECHO):
+    #set GPIO direction (IN / OUT)
+    GPIO.setup(TRIGGER, GPIO.OUT)
+    GPIO.setup(ECHO, GPIO.IN)
+ 
+def distance(TRIGGER, ECHO):
+    # set Trigger to HIGH
+    GPIO.output(TRIGGER, True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(TRIGGER, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(ECHO) == 0:
+        StopTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(ECHO) == 1:
+        StopTime = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+ 
+    return distance
+ 
+if __name__ == '__main__':
+    try:
+        while True:
+            dist = distance(GPIO_TRIGGER, GPIO_ECHO)
+            print (f"Measured Distance = {dist} cm")
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
